637a162c95c89e238f53ebbcffbb377737efddf5
Unknown <john@epioneer.com>
2018-04-02 15:51:29 -0400

    Intoduction page

--- src/pages/introductions/introductions.ts
+++ src/pages/introductions/introductions.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { IntroEmilyPage }from '../intro-emily/intro-emily';
 import { SettingsPage }from '../settings/settings';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the IntroductionsPage page.
@@ -17,9 +18,9 @@ import { SettingsPage }from '../settings/settings';
 })
 export class IntroductionsPage {
 	
-	intros: string = "myhitchm";
+  intros: string = "myhitchm";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 	
   pushToIntroEmily() {
@@ -30,5 +31,26 @@ export class IntroductionsPage {
 	this.navCtrl.push(SettingsPage);
 }
 
-}
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Delete this introduction?',
+      message: 'Are you sure you want to permanently delete this introduction?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 
+}
