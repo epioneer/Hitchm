7fe1aa81c200e85ba36b55fd8e58195f975b8496
Unknown <john@epioneer.com>
2018-04-02 21:30:26 -0400

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { LoginPage } from '../pages/login/login'; 
 import { IntroductionsPage } from '../pages/introductions/introductions';
 import { ChatPage } from '../pages/chat/chat';
 import { AddPage } from '../pages/add/add';
 import { SettingsPage } from '../pages/settings/settings';
 import { IntroEmilyPage } from '../pages/intro-emily/intro-emily'; 
-import { TabsPage } from '../pages/tabs/tabs'; 
+import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ChatPage,
     AddPage,
     SettingsPage,
-    IntroEmilyPage
+    IntroEmilyPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -35,7 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ChatPage,
     AddPage,
     SettingsPage,
-    IntroEmilyPage
+    IntroEmilyPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
