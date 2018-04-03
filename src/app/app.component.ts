d4932a9b207ca1432cac99e791a3ee6ec4d013a3
Unknown <john@epioneer.com>
2018-04-03 10:13:07 -0400

    update for weird login page issue.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { LoginPage } from '../pages/login/login';
 import { IntroductionsPage } from '../pages/introductions/introductions';
 import { TabsPage } from '../pages/tabs/tabs';
 
